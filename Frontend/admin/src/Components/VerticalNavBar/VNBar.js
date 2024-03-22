7fb71115ca1670a0ff552804f3070ffc806629f5
Vörös Dominik (SZF_2022_2024) <voros.dominik@diak.szamalk-szalezi.hu>
2024-03-22 15:20:34 +0100

    nav bar styl

--- Frontend/admin/src/Components/VerticalNavBar/VNBar.js
+++ Frontend/admin/src/Components/VerticalNavBar/VNBar.js
@@ -1,7 +1,22 @@
 import './VNBar.css';
 
 function VNBAR() {
-	return <div className="VNBAR"></div>;
+	return <div className="VNBAR">
+	<div className='ProfPic'>
+	 
+	</div>
+	<label>Felhasználó név</label>
+	<div className="options">
+		<ul>
+			<li> <a href="#"><img src='navbar/icons/logout.jpg' alt=''/> Visza a Főoldalra</a></li>
+			<li> <a href="#"> Beállítások</a></li>
+			<li> <a href="#"> Kijelentkezés</a></li>
+			
+		</ul>
+
+	</div>
+	</div>;
+
 }
 
 export default VNBAR;
