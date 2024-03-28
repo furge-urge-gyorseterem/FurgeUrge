54f74f483ec4f81aa3a3df559cabd8f25621d1bb
VorosDominik <medoxidhun@gmail.com>
2024-03-28 13:41:43 +0100

    oszhang2

--- Frontend/admin/src/api/MealApi.js
+++ Frontend/admin/src/api/MealApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-	baseURL: 'http://localhost:8000/api'
+	baseURL: 'http://localhost:3000/api'
 });
 
 const getMeals = () => httpClient.get('/eteleink');
